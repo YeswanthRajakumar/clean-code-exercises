14f71cc9db77213543a968f399fcc281d403dce1
Yeswanth Rajakumar <yeswanth.rajakumarthoughtworks.com@Yeswanths-MacBook-Pro.local>
2022-06-01 09:09:26 +0530

    add. implementation for TextHelper methods

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -2,11 +2,24 @@ package com.b.simple.design.business.text;
 
 public class TextHelper {
 
-	public String swapLastTwoCharacters(String str) {
-		return null;
-	}
+    public String swapLastTwoCharacters(String str) {
+        if (str.length() < 2) return str;
+        char[] strArray = str.toCharArray();
+        char lastChar = strArray[strArray.length - 1];
+        char secondLastChar = strArray[strArray.length - 2];
+        strArray[strArray.length - 1] = secondLastChar;
+        strArray[strArray.length - 2] = lastChar;
+        return new String(strArray);
+    }
 
-	public String truncateAInFirst2Positions(String str) {
-		return null;
-	}
+    public String truncateAInFirst2Positions(String str) {
+        if (str.length() < 3) return "";
+        StringBuilder s = new StringBuilder(str);
+        if (s.charAt(0) == 'A') s.replace(0, 1, "");
+        if (s.charAt(0) == 'A') s.replace(0, 1, "");
+        if (s.charAt(1) == 'A') s.replace(1, 2, "");
+        return s.toString();
+
+    }
 }
+
